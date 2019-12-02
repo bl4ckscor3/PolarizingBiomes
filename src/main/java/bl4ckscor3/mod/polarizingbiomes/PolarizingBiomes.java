56c5329cf59367a3535c1f43fdaa1e95e21e5747
bl4ckscor3 <bl4ckscor33@gmail.com>
2019-12-02 10:23:33 +0100

    add frozen forest

---
+++ src/main/java/bl4ckscor3/mod/polarizingbiomes/PolarizingBiomes.java
@@ -0,0 +1,36 @@
+package bl4ckscor3.mod.polarizingbiomes;
+
+import bl4ckscor3.mod.polarizingbiomes.biomes.FrozenForest;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.BiomeDictionary;
+import net.minecraftforge.common.BiomeDictionary.Type;
+import net.minecraftforge.common.BiomeManager;
+import net.minecraftforge.common.BiomeManager.BiomeEntry;
+import net.minecraftforge.common.BiomeManager.BiomeType;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+
+@Mod(PolarizingBiomes.MODID)
+@EventBusSubscriber(modid=PolarizingBiomes.MODID, bus=Bus.MOD)
+public class PolarizingBiomes
+{
+	public static final String MODID = "polarizingbiomes";
+
+	@SubscribeEvent
+	public static void onRegisterBiome(RegistryEvent.Register<Biome> event)
+	{
+		//10 is what the vast majority of vanilla biomes use
+		registerBiome(event, new FrozenForest(), "frozen_forest", 10, BiomeType.ICY, Type.COLD, Type.FOREST);
+	}
+
+	private static void registerBiome(RegistryEvent.Register<Biome> event, Biome biome, String registryName, int spawnWeight, BiomeType spawnType, Type... types)
+	{
+		event.getRegistry().register(biome.setRegistryName(new ResourceLocation(MODID, registryName)));
+		BiomeDictionary.addTypes(biome, types);
+		BiomeManager.addBiome(spawnType, new BiomeEntry(biome, spawnWeight));
+	}
+}
---
+++ src/main/java/bl4ckscor3/mod/polarizingbiomes/biomes/FrozenForest.java
@@ -0,0 +1,81 @@
+package bl4ckscor3.mod.polarizingbiomes.biomes;
+
+import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.DefaultBiomeFeatures;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.IFeatureConfig;
+import net.minecraft.world.gen.feature.structure.MineshaftConfig;
+import net.minecraft.world.gen.feature.structure.MineshaftStructure;
+import net.minecraft.world.gen.feature.structure.PillagerOutpostConfig;
+import net.minecraft.world.gen.feature.structure.VillageConfig;
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
+
+public class FrozenForest extends Biome
+{
+	public FrozenForest()
+	{
+		super(new Builder()
+				.surfaceBuilder(SurfaceBuilder.DEFAULT, SurfaceBuilder.GRASS_DIRT_GRAVEL_CONFIG)
+				.precipitation(RainType.SNOW)
+				.category(Category.ICY)
+				.depth(0.075F)
+				.scale(0.04F)
+				.temperature(0.0F)
+				.downfall(0.5F)
+				.waterColor(0x3F76E4)
+				.waterFogColor(0x050533)
+				.parent(null));
+
+		addStructure(Feature.VILLAGE, new VillageConfig("village/snowy/town_centers", 6));
+		addStructure(Feature.IGLOO, IFeatureConfig.NO_FEATURE_CONFIG);
+		addStructure(Feature.MINESHAFT, new MineshaftConfig(0.004D, MineshaftStructure.Type.NORMAL));
+		addStructure(Feature.STRONGHOLD, IFeatureConfig.NO_FEATURE_CONFIG);
+		addStructure(Feature.PILLAGER_OUTPOST, new PillagerOutpostConfig(0.004D));
+
+		DefaultBiomeFeatures.addCarvers(this);
+		DefaultBiomeFeatures.addStructures(this);
+		DefaultBiomeFeatures.addMonsterRooms(this);
+		DefaultBiomeFeatures.addStoneVariants(this);
+		DefaultBiomeFeatures.addOres(this);
+		DefaultBiomeFeatures.addSedimentDisks(this);
+		DefaultBiomeFeatures.addOakAndSpruceTrees(this);
+		DefaultBiomeFeatures.addSprings(this);
+		DefaultBiomeFeatures.addDefaultFlowers(this);
+		DefaultBiomeFeatures.addSparseGrass(this);
+		DefaultBiomeFeatures.addFreezeTopLayer(this);
+
+		addSpawn(EntityClassification.CREATURE, new Biome.SpawnListEntry(EntityType.RABBIT, 10, 2, 3));
+		addSpawn(EntityClassification.CREATURE, new Biome.SpawnListEntry(EntityType.POLAR_BEAR, 1, 1, 2));
+		addSpawn(EntityClassification.AMBIENT, new Biome.SpawnListEntry(EntityType.BAT, 10, 8, 8));
+		addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.SPIDER, 100, 4, 4));
+		addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.ZOMBIE, 95, 4, 4));
+		addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.ZOMBIE_VILLAGER, 5, 1, 1));
+		addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.CREEPER, 100, 4, 4));
+		addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.SLIME, 100, 4, 4));
+		addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.ENDERMAN, 10, 1, 4));
+		addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.WITCH, 5, 1, 1));
+		addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.SKELETON, 20, 4, 4));
+		addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.STRAY, 80, 4, 4));
+	}
+
+	@Override
+	public int getGrassColor(BlockPos pos)
+	{
+		return 0xFEFEFE;
+	}
+
+	@Override
+	public int getFoliageColor(BlockPos pos)
+	{
+		return 0xFEFEFE;
+	}
+
+	@Override
+	public float getSpawningChance()
+	{
+		return 0.07F;
+	}
+}
