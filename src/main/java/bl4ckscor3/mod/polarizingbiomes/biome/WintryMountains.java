f17e41625b14663a836ae4c9c5e3287061d16d97
bl4ckscor3 <bl4ckscor33@gmail.com>
2019-12-13 20:19:42 +0100

    add wintry mountains

---
+++ src/main/java/bl4ckscor3/mod/polarizingbiomes/biome/WintryMountains.java
@@ -0,0 +1,65 @@
+package bl4ckscor3.mod.polarizingbiomes.biome;
+
+import bl4ckscor3.mod.snowundertrees.SnowUnderTrees;
+import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.EntityType;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.DefaultBiomeFeatures;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.IFeatureConfig;
+import net.minecraft.world.gen.feature.structure.MineshaftConfig;
+import net.minecraft.world.gen.feature.structure.MineshaftStructure;
+import net.minecraft.world.gen.feature.structure.PillagerOutpostConfig;
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
+
+public class WintryMountains extends Biome
+{
+	public WintryMountains()
+	{
+		super(new Builder()
+				.surfaceBuilder(SurfaceBuilder.DEFAULT, SurfaceBuilder.GRASS_DIRT_GRAVEL_CONFIG)
+				.precipitation(RainType.SNOW)
+				.category(Category.ICY)
+				.depth(2.0F)
+				.scale(0.6F)
+				.temperature(-0.5F)
+				.downfall(0.5F)
+				.waterColor(0x3F76E4)
+				.waterFogColor(0x050533)
+				.parent(null));
+
+		addStructure(Feature.IGLOO, IFeatureConfig.NO_FEATURE_CONFIG);
+		addStructure(Feature.MINESHAFT, new MineshaftConfig(0.004D, MineshaftStructure.Type.NORMAL));
+		addStructure(Feature.STRONGHOLD, IFeatureConfig.NO_FEATURE_CONFIG);
+		addStructure(Feature.PILLAGER_OUTPOST, new PillagerOutpostConfig(0.004D));
+
+		DefaultBiomeFeatures.addCarvers(this);
+		DefaultBiomeFeatures.addStructures(this);
+		DefaultBiomeFeatures.addMonsterRooms(this);
+		DefaultBiomeFeatures.addStoneVariants(this);
+		DefaultBiomeFeatures.addOres(this);
+		DefaultBiomeFeatures.addSedimentDisks(this);
+		DefaultBiomeFeatures.addScatteredSpruceTrees(this);
+		DefaultBiomeFeatures.addTaigaConifers(this);
+		DefaultBiomeFeatures.addSprings(this);
+		DefaultBiomeFeatures.addExtraEmeraldOre(this);
+		DefaultBiomeFeatures.addInfestedStone(this);
+		DefaultBiomeFeatures.addDefaultFlowers(this);
+		DefaultBiomeFeatures.addSparseGrass(this);
+		DefaultBiomeFeatures.addFreezeTopLayer(this);
+		SnowUnderTrees.addSnowUnderTrees(this);
+
+		addSpawn(EntityClassification.CREATURE, new Biome.SpawnListEntry(EntityType.RABBIT, 10, 2, 3));
+		addSpawn(EntityClassification.CREATURE, new Biome.SpawnListEntry(EntityType.POLAR_BEAR, 1, 1, 2));
+		addSpawn(EntityClassification.AMBIENT, new Biome.SpawnListEntry(EntityType.BAT, 10, 8, 8));
+		addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.SPIDER, 100, 4, 4));
+		addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.ZOMBIE, 95, 4, 4));
+		addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.ZOMBIE_VILLAGER, 5, 1, 1));
+		addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.CREEPER, 100, 4, 4));
+		addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.SLIME, 100, 4, 4));
+		addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.ENDERMAN, 10, 1, 4));
+		addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.WITCH, 5, 1, 1));
+		addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.SKELETON, 20, 4, 4));
+		addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.STRAY, 80, 4, 4));
+	}
+}
